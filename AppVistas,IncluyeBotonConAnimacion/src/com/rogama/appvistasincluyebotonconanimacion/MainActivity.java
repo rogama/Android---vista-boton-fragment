d9bd90c42a78e83da772d91a2337958ef29da5d8
Roberto <gascon87@gmail.com>
2014-05-15 15:48:44 +0200

    asignacion de librerias rotas

--- AppVistas,IncluyeBotonConAnimacion/src/com/rogama/appvistasincluyebotonconanimacion/MainActivity.java
+++ AppVistas,IncluyeBotonConAnimacion/src/com/rogama/appvistasincluyebotonconanimacion/MainActivity.java
@@ -1,9 +1,9 @@
 package com.rogama.appvistasincluyebotonconanimacion;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
